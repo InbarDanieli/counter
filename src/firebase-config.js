fdf2b85a5be7c57b619c9ef8441a5ba5d717ca90
InbarDanieli <inbar0308@gmail.com>
2022-04-29 09:52:20 +0300

    feat: firebase

---
+++ src/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA5MrL0aqt9z5Wf5wR08stMbDYATLijwyE",
+  authDomain: "counter-4u.firebaseapp.com",
+  projectId: "counter-4u",
+  storageBucket: "counter-4u.appspot.com",
+  messagingSenderId: "550535733902",
+  appId: "1:550535733902:web:678e12407891be49db1fdf"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
